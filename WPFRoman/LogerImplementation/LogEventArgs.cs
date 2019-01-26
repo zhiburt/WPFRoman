dd9d943052fe550999ba57fef216b1927ad70ad4
Maxim Zhiburt <zhiburt@gmail.com>
2019-01-27 01:18:03 +0300

    add loger

---
+++ WPFRoman/LogerImplementation/LogEventArgs.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFRoman.Log
+{
+    class LogEventArgs : EventArgs
+    {
+        public LogEventArgs(string mss)
+        {
+            Mss = mss;
+        }
+
+        public string Mss { get; }
+    }
+}
---
+++ WPFRoman/LogerImplementation/Logger.cs
@@ -0,0 +1,71 @@
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFRoman.Log
+{
+    public class Logger
+    {
+        const string Version = "0.0.1";
+        private static Serilog.Core.Logger loger;
+
+        private static readonly Lazy<Logger> lazy =
+            new Lazy<Logger>(() => new Logger());
+
+        public static Logger Log { get => lazy.Value; }
+
+        static Logger()
+        {
+            loger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.WithProperty(nameof(Version), Version)
+                .WriteTo.File(new CompactJsonFormatter(), "logs/log-.json", rollingInterval: RollingInterval.Minute)
+                .WriteTo.ColoredConsole(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+                .CreateLogger();
+        }
+
+        private Logger()
+        {
+        }
+
+        public void Warning(string mss)
+        {
+            loger.Warning(mss);
+            RaiseEvent(nameof(Logger), mss);
+        }
+
+        public void Debug(string mss)
+        {
+            loger.Debug(mss);
+            RaiseEvent(nameof(Logger), mss);
+        }
+
+        public void Information(string mss)
+        {
+            loger.Information(mss);
+            RaiseEvent(nameof(Logger), mss);
+        }
+
+        public void Erorr(string mss)
+        {
+            loger.Error(mss);
+            RaiseEvent(nameof(Logger), mss);
+        }
+
+        public event EventHandler<string> SubLog;
+        public void RaiseEvent(object o, string inf)
+        {
+            SubLog?.Invoke(o, inf);
+        }
+
+        ~Logger()
+        {
+        }
+    }
+}
