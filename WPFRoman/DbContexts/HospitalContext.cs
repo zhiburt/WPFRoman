40f8feb36f8d0dd8909be6e933b7f6be39693908
Maxim Zhiburt <zhiburt@gmail.com>
2019-01-26 16:45:16 +0300

    Add Contexts

---
+++ WPFRoman/DbContexts/HospitalContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFRoman.Models;
+
+namespace WPFRoman.DbContexts
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext() : base("DefaultConnection")
+        {
+        }
+
+        public HospitalContext(string conntectStr) : base(conntectStr)
+        {
+            this.Doctors.Load();
+            this.Patients.Load();
+            this.DoctorsShedules.Load();
+        }
+
+        public virtual DbSet<Doctor> Doctors { get; set; }
+        public virtual DbSet<Patient> Patients { get; set; }
+        public virtual DbSet<DoctorsShedule> DoctorsShedules { get; set; }
+
+        public virtual Patient GetPatientOrCreateByCode(Patient patient)
+        {
+            var pat = Patients.FirstOrDefault(p => p.PassportCode.Equals(patient.PassportCode));
+            if (pat == null)
+            {
+                pat = Patients.Add(patient);
+
+                this.SaveChanges();
+            }
+
+            return pat;
+        }
+
+        public static async Task<HospitalContext> LoadHospitalContext(string str)
+        {
+            var h = new HospitalContext(str);
+
+            await h.Doctors.LoadAsync();
+            await h.Patients.LoadAsync();
+            await h.DoctorsShedules.LoadAsync();
+
+            return h;
+        }
+
+        public ObservableCollection<Doctor> LoadDoctors
+        {
+            get
+            {
+                this.Doctors.Load();
+                return this.Doctors.Local;
+            }
+        }
+    }
+}
---
+++ WPFRoman/DbContexts/HospitalDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFRoman.DbContexts
+{
+    public class HospitalDb
+    {
+        private const string connectionToHospitalContext = "DefaultConnection";
+
+        public HospitalDb(string conn)
+        {
+            Hospital = new HospitalContext(conn);
+        }
+
+        public HospitalDb() : this(connectionToHospitalContext)
+        {
+        }
+
+        public virtual HospitalContext Hospital { get; set; }
+    }
+}
