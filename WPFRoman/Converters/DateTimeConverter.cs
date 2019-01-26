eefb6da37a3a49c4a33b09b6e1fd385e8ac26e1e
Maxim Zhiburt <zhiburt@gmail.com>
2019-01-26 16:45:30 +0300

    Add Converters

---
+++ WPFRoman/Converters/DateTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFRoman.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateTime)value).ToUniversalTime().ToString("f");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ WPFRoman/Converters/NewDoctorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using WPFRoman.ViewModels.Doctors;
+
+namespace WPFRoman.Converters
+{
+    public class NewDoctorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var q = Models.Qualification.Intern;
+            Enum.TryParse((string)values[2], out q);
+
+           return new DoctorViewModel()
+            {
+                Name = (string)values[0],
+                Surname = (string)values[1],
+                Qualification = q,
+            };
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
